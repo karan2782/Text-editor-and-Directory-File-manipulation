689d2d12f61f81b76d915ee73cb93735c3259084
Karan Tanwar <121143712+karan2782@users.noreply.github.com>
2023-10-20 10:21:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+path = 'G:\PythonProjects\File_Handling'
+
+
+def create_dir(dirname):
+    os.mkdir(dirname)
+
+
+def create_file(dirname, filename):
+    os.chdir(dirname)
+    with open(filename, "w") as f:
+        f.write("I am Karan Tanwar.\nI live in India.")
+
+def append_file(filename, text):
+    with open(filename, "a") as f:
+        f.write(text)
+
+        
+def read_file(filename):
+    with open(filename, "r") as f:
+        print(f.read())
+
+def delete_file(filename):
+    os.remove(filename)
+
+def rename_file(filename, new_filename):
+    os.rename(filename, new_filename)
+
+def give_path_and_filename(dirname, filename):
+    with open(filename, "w") as f:
+        f.write("I am Karan Tanwar.\nI live in India.")
+
+    print(f"\nContent of {dirname}:")
+    print(f"File: The key components of an enterprise levels {filename}\n")
+
+
+
+while True:
+    
+    print("Options:")
+    print("1. List files and directories")
+    print("2. Create a directory")
+    print("3. Delete a file")
+    print("4. Rename a file")
+    print("5. Exit")
+    n = int(input("Enter your choice: "))
+    print()
+
+    if n==1:
+        create_file(input("Enter dirname: "), input("Enter filename: "))
+    
+    elif n==2:
+        create_dir(input("Enter name of directory: "))
+
+    elif n==3:
+        delete_file(input("Enter name of file which you want to delete: "))
+    
+    elif n==4:
+        rename_file(input("Enter current file name: "), input("Enter new name of file: "))
+    elif n==5:
+        break
+
+
+
