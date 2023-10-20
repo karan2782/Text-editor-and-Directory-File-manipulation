f47705725d952e2d3bf6aa874db11886e9e4c6e5
Karan Tanwar <121143712+karan2782@users.noreply.github.com>
2023-10-20 10:24:32 +0530

    main.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import os
 path = 'G:\PythonProjects\File_Handling'
 
-
+# here  creating  a new directory
 def create_dir(dirname):
     os.mkdir(dirname)
 
-
+# create file inside direcotry
 def create_file(dirname, filename):
     os.chdir(dirname)
     with open(filename, "w") as f:
         f.write("I am Karan Tanwar.\nI live in India.")
 
+# append text inside file by this function
 def append_file(filename, text):
     with open(filename, "a") as f:
         f.write(text)
 
-        
+# read file by this function      
 def read_file(filename):
     with open(filename, "r") as f:
         print(f.read())
-
+        
+#delete file by this function
 def delete_file(filename):
     os.remove(filename)
 
+# renaming of fine by this function
 def rename_file(filename, new_filename):
     os.rename(filename, new_filename)
 
+# here we create file and show 
 def give_path_and_filename(dirname, filename):
     with open(filename, "w") as f:
         f.write("I am Karan Tanwar.\nI live in India.")
